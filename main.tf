c7d2c714dc5411c140e027592c1004f6ea3cec31
A. Can Gencdogus <a.cangencdogus@As-MacBook-Air.local>
2025-05-05 21:15:43 -0400

    making a change

--- main.tf
+++ main.tf
@@ -194,6 +194,7 @@ resource "aws_launch_template" "default" {
   }
   user_data = base64encode(<<-EOF
               #!/bin/bash
+              set -eu
               sudo yum update -y
               sudo yum install -y httpd
               sudo systemctl start httpd.service
